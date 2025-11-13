79ff9ae92febf7922d825fd6b13347d740284be8
InnoVi Limited <35452047+innoviai@users.noreply.github.com>
2025-11-13 17:26:32 +0800

    Add files via upload

---
+++ src/utils/ServiceURL.tsx
@@ -0,0 +1,6 @@
+//export const baseUrl: string = "http://localhost/react_server/";
+//export const baseUrl: string = "http://16.162.163.90/";
+
+//export const baseUrl: string = "http://localhost/growntech/mvp/v2/module/erp_final/php-superAdmin&admin/";
+export const baseUrl: string = "https://grown.tech/adminpanel/rooftop/";
+
---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+//export const ip = "http://localhost:8000/";
+//export const ip = "https://grown.tech/generativeai/";
+
+// Primary IP for data display
+export const DATA_DISPLAY_IP = "http://localhost:8000/"; 
+//export const DATA_DISPLAY_IP = "https://grown.tech/generativeai/"; 
+
+// Secondary IP for data display
+export const GENAI_ENDPOINT  = "http://localhost:8000";
+//export const GENAI_ENDPOINT  = "https://4337-45-64-240-199.ngrok-free.app/";
